f991f56e5f43e66985e94e1d3430a4c66d5919e6
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-06-11 17:45:44 +0530

    Added a button with button changed data and rendered

--- App.jsx
+++ App.jsx
@@ -28,52 +28,80 @@ class TableRow extends React.Component {
     }
 }
 
-class Content extends React.Component {
+class Inputbox extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: 'Your Name'
+        }
+
+        this.nameChanged = this.nameChanged.bind(this);
+
+    };
+
+    nameChanged(e) {
+        this.setState({data: e.target.value});
+    }
+
     render() {
         return (
             <div>
-                {this.props.contentProp.map((contentList, i) => < TableRow key = {
-                    i
-                }
-                data = {
-                    contentList
-                } />)}
+                <input type="text" value={this.state.data} onChange={this.nameChanged}/>
+                <h4>Hi {this.state.data}!</h4>
             </div>
         );
     }
 }
 
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <Inputbox/> {this.props.contentProp.map((contentList, i) => < TableRow key = {
+                    i
+                }
+                data = {
+                    contentList
+                } />)}
+                <Button/>
+            </div>
+
+        );
+    }
+}
+
 class Button extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data : []
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+
+        this.buttonClicked = this.buttonClicked.bind(this)
     }
 
-    this.buttonClicked = this.buttonClicked.bind(this)
-  }
+    buttonClicked() {
+        var item = "buttonClicked..."
+        var myArray = this.state.data;
+        myArray.push(item)
+        this.setState({data: myArray})
+    };
 
-  buttonClicked() {
-      var item = "buttonClicked..."
-      var myArray = this.state.data;
-      myArray.push(item)
-      this.setState({data: myArray})
-   };
-
-   render() {
-      return (
-         <div>
-            <button onClick = {this.buttonClicked}>BUTTON</button>
-            <h4>{this.state.data}</h4>
-         </div>
-      );
-   }
+    render() {
+        return (
+            <div>
+                <button onClick={this.buttonClicked}>BUTTON</button>
+                <h4>{this.state.data}</h4>
+            </div>
+        );
+    }
 }
 
 class Page extends React.Component {
     constructor() {
-      super();
-        this.header = "This is a basic React App Header got from prop"
+        super();
+        this.header = "This is a basic React App!"
         this.listOfContent = {
             data: [
                 {
@@ -89,9 +117,9 @@ class Page extends React.Component {
     render() {
         return (
             <div>
-                <Header headerProp= {this.header}/>
-                <Content contentProp= {this.listOfContent.data}/>
-                <Button/>
+                <Header headerProp={this.header}/>
+                <Content contentProp={this.listOfContent.data}/>
+
             </div>
         );
     }
